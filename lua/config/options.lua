e8e04835d715816ea0c4da4baea4c2f9309016d1
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2026-01-07 15:17:31 +0200

    Add wrapline traversal and moving lines
    Adds keymaps for wrap line traversal and moving lines with alt.
    Small changes to tab widths, changed them to default to four.
    Changed autocompletion from ctrl-y to enter.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,11 @@
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.shiftwidth = 4 -- Tab behaviour
+
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 vim.o.mouse = 'a'
 vim.o.showmode = false --Don't show the mode, since it's already in the status line
