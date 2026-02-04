3c6b3c89c72a48059146213d3b88b78afa24ea9c
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2026-02-04 13:59:28 +0200

    Prettierd deals with TypescriptReact

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -31,6 +31,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       typescript = { 'prettierd' },
+      typescriptreact = { 'prettierd' },
       -- Conform can also run multiple formatters sequentially
       python = { 'isort', 'black' },
       --
